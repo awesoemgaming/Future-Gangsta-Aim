1aa5ded8f767ac98673e67f4d67b6d8e6e3fecbe
awesoemgaming <129987735+awesoemgaming@users.noreply.github.com>
2023-05-10 20:53:33 -0700

    Update Config.lua

--- gangsta_aim/Config.lua
+++ gangsta_aim/Config.lua
@@ -1,3 +1,6 @@
 Config = {}
 
 Config.core = "qb" -- Change this for whatever you use. 'qb' or 'esx'
+Config.aimanimdict = "combat@aim_variations@1h@gang" -- the aniamtion dictionary for the aim aniamtion.
+Config.aimanimdicttype = "aim_variation_a" -- animation type
+-- See https://gtahash.ru/animations/ for more info about aniamtion dict/type
