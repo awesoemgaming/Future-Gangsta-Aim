df6210148dcccaf3c41c03d5f946086787b1d773
awesoemgaming <129987735+awesoemgaming@users.noreply.github.com>
2023-05-10 19:11:21 -0700

    Create cl.lua

---
+++ gangsta_aim/cl.lua
@@ -0,0 +1,46 @@
+Future = nil
+local gangstaAimEnabled = false
+
+Citizen.CreateThread(function()
+    while Future == nil do
+        TriggerEvent('QBCore:GetObject', function(obj) Future = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
+RegisterNetEvent('Future_gangaim:toggle')
+AddEventHandler('Future_gangaim:toggle', function()
+    gangstaAimEnabled = not gangstaAimEnabled
+
+    if not gangstaAimEnabled then
+        local playerPed = GetPlayerPed(-1)
+        ClearPedSecondaryTask(playerPed)
+        SetEnableHandcuffs(playerPed, false)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(1, 38) then
+            TriggerEvent('Future_gangaim:toggle')
+        end
+
+        if gangstaAimEnabled then
+            local playerPed = GetPlayerPed(-1)
+
+            if IsPedArmed(PlayerPedId(), 7) and not IsPlayerFreeAiming(PlayerId()) then
+                ClearPedSecondaryTask(playerPed)
+                SetEnableHandcuffs(playerPed, false)
+            elseif IsPlayerFreeAiming(PlayerId()) and not IsEntityPlayingAnim(playerPed, "combat@aim_variations@1h@gang", "aim_variation_a", 3) then
+                RequestAnimDict("combat@aim_variations@1h@gang")
+                while (not HasAnimDictLoaded("combat@aim_variations@1h@gang")) do
+                    Citizen.Wait(100)
+                end
+
+                SetEnableHandcuffs(playerPed, true)
+                TaskPlayAnim(playerPed, "combat@aim_variations@1h@gang", "aim_variation_a", 8.0, 2.5, -1, 49, 0, 0, 0, 0)
+            end
+        end
+    end
+end)
